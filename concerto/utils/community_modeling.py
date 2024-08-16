96a8bcdc8f6ea89651c50dcce1cf70dea39004d6
August <30163079+augeorge@users.noreply.github.com>
2024-08-15 18:06:55 -0700

    created seperate notebooks for smetana community and caco3 modeling, created community_modeling.py for util functions, removed extra files

---
+++ concerto/utils/community_modeling.py
@@ -0,0 +1,93 @@
+import numpy as np
+import tellurium as te
+import pandas as pd
+import libsbml
+from mira.metamodel import Distribution
+from mira.modeling.amr.petrinet import AMRPetriNetModel
+from mira.modeling import Model
+from pathlib import Path
+
+
+
+def save_SBML_from_smetana_df(smetana_df, seed=0, initial_bounds=(0,1), smbl_file='test_SBML.xml'):
+    """
+    Create an Antimony string based on the data in smetana_df and save it as an SBML (.xml) file.
+
+    For each row of the smetana data:
+    - donor-to-compound reaction: donor -> compound; rate: k_d*donor    
+    - compound-to-receiver reaction: compound -> receiver; k_r*compound
+    - use uniform initial values for the model variables (donor, compound, receiver)
+    - k_d = scs and k_r = mus for that row 
+    """
+    np.random.seed(seed)
+
+    reaction_list = []
+    parameter_list = []
+    initial_values_set = set()
+
+    for index, row in smetana_df.iterrows():
+        donor_reaction = f'\t{row.donor} -> {row.compound}; k_d{index}*{row.donor}'
+        receiver_reaction = f'\t{row.compound} -> {row.receiver}; k_r{index}*{row.compound}'
+        reaction_list.extend([donor_reaction, receiver_reaction])
+        parameter_list.extend([f'\tk_d{index}={row.scs}', f'\tk_r{index}={row.mus}'])
+        initial_values_set.add(row.donor)
+        initial_values_set.add(row.compound)
+        initial_values_set.add(row.receiver)
+
+    initial_values_string = "\n".join([f"\t{name}={np.random.uniform(initial_bounds[0],initial_bounds[1])}" for name in initial_values_set])
+    species_names_string = "\n".join([f'\t{species} is "{species}"' for species in initial_values_set])
+
+    reaction_string = "\n".join(reaction_list)
+    parameter_string = "\n".join(parameter_list)
+
+    antimony_str = f"""
+    model smetana
+    {reaction_string}
+    {parameter_string}
+    {initial_values_string}
+    {species_names_string}
+    end
+    """
+    
+    rr_model = te.loada(antimony_str)
+    rr_model.exportToSBML(smbl_file)
+
+
+def update_SBML_for_MIRA(original_sbml_file, updated_sbml_file):
+    """Updates the SBML file to ensure that the species name, initial concentrations, and has_only_substance_units are set."""
+    d = libsbml.readSBMLFromFile(original_sbml_file)
+    m = d.getModel()
+    for species in m.getListOfSpecies():
+        species.name = species.id
+        species.initial_concentration = species.initial_amount
+        species.has_only_substance_units = False
+
+    libsbml.writeSBMLToFile(d,updated_sbml_file)
+
+
+def set_uniform_priors_for_MIRA_model_rate_constants(mira_model, min=1e-3, max=1):
+    "Assigns a uniform(min,max) distribution for rate constants (parameters starting with 'k')."
+
+    prior_dist = Distribution(
+                    type= "Uniform1",
+                    parameters= {"minimum": min,"maximum": max }     
+                     )
+
+    for name,parameter in mira_model.parameters.items():
+        if name.startswith('k'): # only assign priors for rate constants
+            parameter.distribution = prior_dist
+    return mira_model
+
+
+def convert_MIRA_model_to_petrinet_AMRPetriNetModel(mira_model, petrinet_model_filename):
+    "Converts a MIRA model into a PetriNet Model (.JSON) and saves it."
+    # Create a Path object and remove extension
+    file_path = Path(petrinet_model_filename)
+    file_root = file_path.stem
+    petrinet_model = AMRPetriNetModel(Model(mira_model))
+    petrinet_model.to_json_file(
+                petrinet_model_filename,
+                name=file_root,
+                description=file_root,
+                indent=2,
+            )
