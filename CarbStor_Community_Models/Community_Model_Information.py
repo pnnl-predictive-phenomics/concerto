c687cde35e3a7399d50b2493c95b5ff7332b9a49
Tesia Lin <140449955+tiekneeshrimp@users.noreply.github.com>
2024-09-09 13:11:59 -0700

    Update Community_Model_Information.py
    Changed to relative (github) filepath

--- CarbStor_Community_Models/Community_Model_Information.py
+++ CarbStor_Community_Models/Community_Model_Information.py
@@ -12,7 +12,7 @@ from micom.workflows import build, grow, tradeoff, fix_medium,build_database
 from micom import load_pickle
 from micom.viz import plot_tradeoff, plot_exchanges_per_sample, plot_growth
 
-com1 = load_pickle("/Users/rodr579/Library/CloudStorage/OneDrive-PNNL/venv/CarbStor_Community/One.pickle")
+com1 = load_pickle("/CarbStor_Community_Models/One.pickle")
 result1 = com1.optimize(fluxes=True, pfba=True)
 #result1
 result1.summary()
@@ -21,7 +21,7 @@ result1.summary()
 # In[4]:
 
 
-com2 = load_pickle("/Users/rodr579/Library/CloudStorage/OneDrive-PNNL/venv/CarbStor_Community_NoCurtobacterium/One.pickle")
+com2 = load_pickle("CarbStor_Community_NoCurtobacterium/One.pickle")
 result2 = com2.optimize(fluxes=True, pfba=True)
 #result2
 result2.summary()
