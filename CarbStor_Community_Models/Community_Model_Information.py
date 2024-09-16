f3e80206c95f943caa0bb69b01d01c87f56e45f6
tiekneeshrimp <tayshlin@gmail.com>
2024-09-16 13:06:07 -0700

    removed jupyter notebook cells, defined function main(), removed extra imports, redefined variables

--- CarbStor_Community_Models/Community_Model_Information.py
+++ CarbStor_Community_Models/Community_Model_Information.py
@@ -9,35 +9,26 @@ import pandas as pd
 from cobra.io import read_sbml_model
 from pathlib import Path
 
-from micom import Community
-from micom.workflows import build, grow, tradeoff, fix_medium,build_database
 from micom import load_pickle
-from micom.viz import plot_tradeoff, plot_exchanges_per_sample, plot_growth
 
 
 #set variables for importing data
 HERE = Path(__file__).parent.resolve()
-ROOT = HERE.parent.resolve()
-COM_MODEL = HERE.joinpath("One.pickle")
-NO_CURTO = ROOT.joinpath("CarbStor_Community_NoCurtobacterium/One.pickle")
+COM_MODEL = HERE.joinpath("Cabstor_Community/One.pickle")
+NO_CURTO = HERE.joinpath("CarbStor_Community_NoCurtobacterium/One.pickle")
 
 #load communities and define parameters
-com1 = load_pickle(COM_MODEL)
-result1 = com1.optimize(fluxes=True, pfba=True)
-#result1
-result1.summary()
+def main():
+    com1 = load_pickle(COM_MODEL)
+    result1 = com1.optimize(fluxes=True, pfba=True)
+    #result1
+    print(result1.members)
 
-
-# In[4]:
-
-com2 = load_pickle(NO_CURTO)
-result2 = com2.optimize(fluxes=True, pfba=True)
-#result2
-result2.summary()
-
-
-# In[ ]:
-
-
-
+    com2 = load_pickle(NO_CURTO)
+    result2 = com2.optimize(fluxes=True, pfba=True)
+    #result2
+    result2.summary()
+    if __name__ == "main":
+        main()
+    print(result2.members)
 
