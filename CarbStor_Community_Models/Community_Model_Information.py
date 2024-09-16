b280377bd381c6d57a7703d48f6e5704621ff394
mcnaughtonadm <mcnaughtonadm@gmail.com>
2024-09-16 14:21:23 -0700

    Slight fixes to Community_Model_Information.py

--- CarbStor_Community_Models/Community_Model_Information.py
+++ CarbStor_Community_Models/Community_Model_Information.py
@@ -3,7 +3,7 @@
 
 # In[ ]:
 
-#add in pathlib to make sure everyone can get this data
+# add in pathlib to make sure everyone can get this data
 
 import pandas as pd
 from cobra.io import read_sbml_model
@@ -12,23 +12,22 @@ from pathlib import Path
 from micom import load_pickle
 
 
-#set variables for importing data
+# set variables for importing data
 HERE = Path(__file__).parent.resolve()
-COM_MODEL = HERE.joinpath("Cabstor_Community/One.pickle")
+COM_MODEL = HERE.joinpath("Carbstor_Community/One.pickle")
 NO_CURTO = HERE.joinpath("CarbStor_Community_NoCurtobacterium/One.pickle")
 
-#load communities and define parameters
+
+# load communities and define parameters
 def main():
     com1 = load_pickle(COM_MODEL)
     result1 = com1.optimize(fluxes=True, pfba=True)
-    #result1
     print(result1.members)
 
     com2 = load_pickle(NO_CURTO)
     result2 = com2.optimize(fluxes=True, pfba=True)
-    #result2
-    result2.summary()
-    if __name__ == "main":
-        main()
     print(result2.members)
 
+
+if __name__ == "__main__":
+    main()
